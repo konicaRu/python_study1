a4a9ff172b4be72ce690b5ed4daefcad62e5d2ec
konicaRu <53256405+konicaRu@users.noreply.github.com>
2019-08-13 15:08:12 +0300

    start

---
+++ SumOfThe.py
@@ -0,0 +1,18 @@
+def SumOfThe(N, data):
+    summ = 0
+    court = -1
+    for j in range(len(data)):
+        court += 1
+        for i in range(len(data)):
+            summ += data[i]
+        summ -= data[court]
+        if summ == 0:
+            return data[court]
+        if summ == data[court]:
+            return summ
+        if summ != data[court]:
+            summ = 0
+
+
+data = [10, -25, -25, -85, 5]
+print(SumOfThe(2, data))
