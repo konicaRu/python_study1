e64ef13a31e3f0bda95dbd65d516f326ed1dc148
konicaRu <53256405+konicaRu@users.noreply.github.com>
2019-09-06 14:41:07 +0300

    refrm_code

--- TransformTransform.py
+++ TransformTransform.py
@@ -1,15 +1,15 @@
-def TransformTransform (A,N):
+def TransformTransform(A, N):
     C = []
-    tmp = []#временный массив
+    tmp = []  # временный массив
     for i in A:
-        for j in range(len(A )- (i -1)):
+        for j in range(len(A) - (i - 1)):
             k = i + j
-            if k + 1 > len(A):# если к превысит диапазан массива А
-                k = len(A) - 1# приводим к к длинне массива А
-            for h in range(j, k+1):#перебираем А в заданном диапазоне
-                tmp.append(A[h])# добавляем во временный архив
-            C.append(max(tmp))#добавляем в В макс значение из тмп
-            tmp = []#обнуляем временный массив
+            if k + 1 > len(A):  # если к превысит диапазан массива А
+                k = len(A) - 1  # приводим к к длинне массива А
+            for h in range(j, k + 1):  # перебираем А в заданном диапазоне
+                tmp.append(A[h])  # добавляем во временный архив
+            C.append(max(tmp))  # добавляем в В макс значение из тмп
+            tmp = []  # обнуляем временный массив
     i = 0
     j = 0
     k = 0
@@ -25,5 +25,5 @@ def TransformTransform (A,N):
                 tmp.append(C[h])  # добавляем во временный архив
             B.append(max(tmp))  # добавляем в В макс значение из тмп
             tmp = []  # обнуляем временный массив
-    if sum(B)%2 == 0:
+    if sum(B) % 2 == 0:
         return True
