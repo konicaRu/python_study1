c9e4b744fa12098bc6aa14a43be389d7af47ee26
konicaRu <53256405+konicaRu@users.noreply.github.com>
2019-09-09 19:21:45 +0300

    del_1

--- TransformTransform.py
+++ TransformTransform.py
@@ -1,12 +1,10 @@
 def TransformTransform(A, N):
     C = []
     tmp = []  # временный массив
-    for i in A:
-        for j in range(len(A) - (i - 1)):
+    for i in range(len(A)):
+        for j in range(0, len(A) - i):
             k = i + j
-            if k + 1 > len(A):  # если к превысит диапазан массива А
-                k = len(A) - 1  # приводим к к длинне массива А
-            for h in range(j, k + 1):  # перебираем А в заданном диапазоне
+            for h in range(j, k+1):  # перебираем А в заданном диапазоне
                 tmp.append(A[h])  # добавляем во временный архив
             C.append(max(tmp))  # добавляем в В макс значение из тмп
             tmp = []  # обнуляем временный массив
@@ -16,14 +14,13 @@ def TransformTransform(A, N):
     h = 0
     B = []
     tmp = []  # временный массив
-    for i in C:
-        for j in range(len(C) - (i - 1)):
+    for i in range(len(C)):
+        for j in range(0, len(C) - i):
             k = i + j
-            if k + 1 > len(C):  # если к превысит диапазан массива А
-                k = len(C) - 1  # приводим к к длинне массива А
             for h in range(j, k + 1):  # перебираем А в заданном диапазоне
                 tmp.append(C[h])  # добавляем во временный архив
             B.append(max(tmp))  # добавляем в В макс значение из тмп
             tmp = []  # обнуляем временный массив
     if sum(B) % 2 == 0:
         return True
+
