8c87cca00b22d6287b2b7db7207b61fb0b38973c
konicaRu <53256405+konicaRu@users.noreply.github.com>
2019-09-12 21:42:44 +0300

    Add files via upload

---
+++ Keymaker.py
@@ -0,0 +1,11 @@
+def Keymaker(k):
+    arr_moff = [0 for i in range(k+1)]
+    count = 0
+    for i in range(len(arr_moff) - 1):
+        count += 1
+        for j in range(0, len(arr_moff), count):
+            if arr_moff[j] == 0:
+                arr_moff[j] = 1
+            else:arr_moff[j] = 0
+    arr = arr_moff[1:]
+    return arr
