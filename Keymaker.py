f392a70ce22eb4f8525953020251024b03f173cf
konicaRu <53256405+konicaRu@users.noreply.github.com>
2019-09-12 21:47:19 +0300

    add_str

--- Keymaker.py
+++ Keymaker.py
@@ -1,6 +1,7 @@
 def Keymaker(k):
     arr_moff = [0 for i in range(k+1)]
     count = 0
+    line = ''
     for i in range(len(arr_moff) - 1):
         count += 1
         for j in range(0, len(arr_moff), count):
@@ -8,4 +9,7 @@ def Keymaker(k):
                 arr_moff[j] = 1
             else:arr_moff[j] = 0
     arr = arr_moff[1:]
-    return arr
+    for i in arr:
+        line += str(i)
+
+    return line
