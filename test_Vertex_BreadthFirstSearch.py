cc41b450a32a6edf2df4a24634dd6f5a56a178e5
konicaRu <53256405+konicaRu@users.noreply.github.com>
2020-01-29 21:13:47 +0300

    Update test_Vertex_BreadthFirstSearch.py

--- test_Vertex_BreadthFirstSearch.py
+++ test_Vertex_BreadthFirstSearch.py
@@ -60,6 +60,27 @@ class MyTestCase(unittest.TestCase):
         self.assertEqual(graff.BreadthFirstSearch(3, 5)[0] == graff.vertex[3], True)
         self.assertEqual(graff.BreadthFirstSearch(3, 5)[-1] == graff.vertex[5], True)
 
+    def test_my_long(self):
+        graff = Vertex_BreadthFirstSearch.SimpleGraph(6)
+        graff.AddVertex(0)
+        graff.AddVertex(1)
+        graff.AddVertex(2)
+        graff.AddVertex(3)
+        graff.AddVertex(4)
+        graff.AddVertex(5)
+        graff.AddEdge(0, 1)
+        graff.AddEdge(1, 2)
+        graff.AddEdge(2, 3)
+        graff.AddEdge(3, 4)
+        graff.AddEdge(4, 5)
+        self.assertEqual(len(graff.BreadthFirstSearch(0, 5)) == 6, True)
+        self.assertEqual(graff.BreadthFirstSearch(0, 5)[0] == graff.vertex[0], True)
+        self.assertEqual(graff.BreadthFirstSearch(0, 5)[1] == graff.vertex[1], True)
+        self.assertEqual(graff.BreadthFirstSearch(0, 5)[2] == graff.vertex[2], True)
+        self.assertEqual(graff.BreadthFirstSearch(0, 5)[3] == graff.vertex[3], True)
+        self.assertEqual(graff.BreadthFirstSearch(0, 5)[4] == graff.vertex[4], True)
+        self.assertEqual(graff.BreadthFirstSearch(0, 5)[-1] == graff.vertex[5], True)
+
 
 
 if __name__ == '__main__':
