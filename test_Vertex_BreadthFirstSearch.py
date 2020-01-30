5fa335133c005afa1e48ff8439e59434ecc46811
konicaRu <53256405+konicaRu@users.noreply.github.com>
2020-01-30 16:31:18 +0300

    30_01_20 16_31

--- test_Vertex_BreadthFirstSearch.py
+++ test_Vertex_BreadthFirstSearch.py
@@ -2,8 +2,9 @@ import unittest
 import Vertex_BreadthFirstSearch
 
 
+class MyTestCase(unittest.TestCase):
     def test_my_peper_B(self):
-        graff = Vertex_BreadthFirstSearch.SimpleGraph(6)
+        graff = Vertex_BreadthFirstSearch.SimpleGraph(5)
         graff.AddVertex(0)
         graff.AddVertex(1)
         graff.AddVertex(2)
@@ -20,8 +21,8 @@ import Vertex_BreadthFirstSearch
         self.assertEqual(graff.BreadthFirstSearch(0, 3)[0] == graff.vertex[0], True)
         self.assertEqual(graff.BreadthFirstSearch(0, 3)[-1] == graff.vertex[3], True)
 
-       def test_my_peper_B2(self):
-        graff = Vertex_BreadthFirstSearch.SimpleGraph(6)
+    def test_my_peper_B2(self):
+        graff = Vertex_BreadthFirstSearch.SimpleGraph(5)
         graff.AddVertex(0)
         graff.AddVertex(1)
         graff.AddVertex(2)
@@ -38,27 +39,10 @@ import Vertex_BreadthFirstSearch
         self.assertEqual(graff.BreadthFirstSearch(0, 4)[0] == graff.vertex[0], True)
         self.assertEqual(graff.BreadthFirstSearch(0, 4)[-1] == graff.vertex[4], True)
         self.assertEqual(graff.BreadthFirstSearch(0, 4)[1] == graff.vertex[1], True)
- 
-    def test_my_peper(self):
-        graff = Vertex_BreadthFirstSearch.SimpleGraph(6)
-        graff.AddVertex(0)
-        graff.AddVertex(1)
-        graff.AddVertex(2)
-        graff.AddVertex(3)
-        graff.AddVertex(4)
-        graff.AddVertex(5)
-        graff.AddEdge(0, 1)
-        graff.AddEdge(0, 2)
-        graff.AddEdge(0, 3)
-        graff.AddEdge(4, 2)
-        graff.AddEdge(1, 2)
-        graff.AddEdge(2, 3)
-        graff.AddEdge(3, 4)
-        self.assertEqual(len(graff.BreadthFirstSearch(0, 4)) == 3, True)
-        self.assertEqual(graff.BreadthFirstSearch(0, 4)[0] == graff.vertex[0], True)
-        self.assertEqual(graff.BreadthFirstSearch(0, 4)[-1] == graff.vertex[4], True)
-        self.assertEqual(graff.BreadthFirstSearch(0, 4)[1] == graff.vertex[2], True)
-        self.assertEqual(len(graff.BreadthFirstSearch(0, 5)) == 0, True)
+        self.assertEqual(len(graff.BreadthFirstSearch(0, 3)) == 2, True)
+        self.assertEqual(graff.BreadthFirstSearch(0, 3)[0] == graff.vertex[0], True)
+        self.assertEqual(graff.BreadthFirstSearch(0, 3)[-1] == graff.vertex[3], True)
+
 
     def test_my_peper2(self):
         graff = Vertex_BreadthFirstSearch.SimpleGraph(6)
