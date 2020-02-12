561f98e05bf38c1c086f98264e1c9e5e922e7e3c
konicaRu <53256405+konicaRu@users.noreply.github.com>
2020-02-12 10:39:31 +0300

    12_02_20

--- ArrayChunk.py
+++ ArrayChunk.py
@@ -8,12 +8,22 @@ def ArrayChunk(m):
             i1 += 1
         if m[i2] > n:
             i2 -= 1
+        if i1 == i2 - 1 and m[i1] > m[i2]:
+            inter_el = m[i1]
+            m[i1] = m[i2]
+            m[i2] = inter_el
+            n = m[len(m) // 2]
+            ind_supp = len(m) // 2
+            i1 = 0
+            i2 = len(m) - 1
+            continue
         if i1 == i2 or i1 == i2 - 1 and m[i1] < m[i2]:
-            return ind_supp
-        if m[i1] == n:
-            ind_supp = i2
-        if m[i2] == n:
-            ind_supp = i1
-        inter_el = m[i1]
-        m[i1] = m[i2]
-        m[i2] = inter_el
+            return ind_supp, m
+        if m[i1] >= n and m[i2] <= n:
+            if m[i1] == n:
+                ind_supp = i2
+            if m[i2] == n:
+                ind_supp = i1
+            inter_el = m[i1]
+            m[i1] = m[i2]
+            m[i2] = inter_el
