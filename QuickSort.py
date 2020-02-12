6e54f0795f3893978e97b1fefa3a20a932f91bb8
konicaRu <53256405+konicaRu@users.noreply.github.com>
2020-02-12 22:39:38 +0300

    12_02_20

---
+++ QuickSort.py
@@ -0,0 +1,33 @@
+def QuickSort(m, i1, i2 ):
+    if i1 == i2:
+        return
+    else:
+        left = i1
+        right = i2
+        n = m[(i1 + i2 + 1) // 2]
+        ind_supp = (i1 + i2 + 1) // 2
+        while True:
+            if m[i1] < n:
+                i1 += 1
+            if m[i2] > n:
+                i2 -= 1
+            if i1 == i2 - 1 and m[i1] > m[i2]:
+                m[i1], m[i2] = m[i2], m[i1] # меняем местами элементы списка A[i], A[j] = A[j], A[i]
+                i1 = left
+                i2 = right
+                n = m[(i1 + i2 + 1) // 2]
+                ind_supp = (i1 + i2 + 1) // 2
+                continue
+            if i1 == i2 or i1 == i2 - 1 and m[i1] < m[i2]:
+                QuickSort(m, left, ind_supp - 1 )
+                QuickSort(m, ind_supp + 1, right)
+            if m[i1] >= n and m[i2] <= n:
+                if m[i1] == n:
+                    ind_supp = i2
+                if m[i2] == n:
+                    ind_supp = i1
+                m[i1], m[i2] = m[i2], m[i1] # меняем местами элементы списка A[i], A[j] = A[j], A[i]
+
+
+#print(QuickSort([6, 5, 7], 0, 1))
+print(QuickSort( [5, 9, 4, 7, 1], 0, 4))
