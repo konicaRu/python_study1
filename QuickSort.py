995d1006fd9061b40430f68f6386b1b165424ce0
konicaRu <53256405+konicaRu@users.noreply.github.com>
2020-02-14 11:56:46 +0300

    14_02_20

--- QuickSort.py
+++ QuickSort.py
@@ -1,29 +1,31 @@
-def QuickSort(m, i1, i2 ):
+def QuickSort(m, i1, i2):
     if i1 == i2:
         return
     else:
         left = i1
         right = i2
-        n = m[(i1 + i2 + 1) // 2]
-        ind_supp = (i1 + i2 + 1) // 2
-        while True:
-            if m[i1] < n:
+        if (left + right + 1)//2 > len(m) - 1:
+            return
+        n = m[(left + right + 1) // 2]
+        ind_supp = (left + right + 1) // 2
+        while i1 <= i2:
+            while m[i1] < n:
                 i1 += 1
-            if m[i2] > n:
+            while m[i2] > n:
                 i2 -= 1
             if i1 == i2 - 1 and m[i1] > m[i2]:
-                m[i1], m[i2] = m[i2], m[i1] # меняем местами элементы списка A[i], A[j] = A[j], A[i]
+                m[i1], m[i2] = m[i2], m[i1]  # меняем местами элементы списка A[i], A[j] = A[j], A[i]
+                n = m[(left + right + 1) // 2]
+                ind_supp = (left + right + 1) // 2
                 i1 = left
                 i2 = right
-                n = m[(i1 + i2 + 1) // 2]
-                ind_supp = (i1 + i2 + 1) // 2
                 continue
             if i1 == i2 or i1 == i2 - 1 and m[i1] < m[i2]:
-                QuickSort(m, left, ind_supp - 1 )
-                QuickSort(m, ind_supp + 1, right)
+                QuickSort(m, left, ind_supp - 1)
+                return QuickSort(m, ind_supp + 1, right)
             if m[i1] >= n and m[i2] <= n:
                 if m[i1] == n:
                     ind_supp = i2
                 if m[i2] == n:
                     ind_supp = i1
-                m[i1], m[i2] = m[i2], m[i1] # меняем местами элементы списка A[i], A[j] = A[j], A[i]
+                m[i1], m[i2] = m[i2], m[i1]  # меняем местами элементы списка A[i], A[j] = A[j], A[i]
