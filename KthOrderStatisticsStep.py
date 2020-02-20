308a92248366e5df7ca87c60f8ab2af272ee1ea4
konicaRu <53256405+konicaRu@users.noreply.github.com>
2020-02-20 19:57:18 +0300

    20_02_20

--- KthOrderStatisticsStep.py
+++ KthOrderStatisticsStep.py
@@ -31,10 +31,3 @@ def InsertionSort(arr, left, right):
             arr[j + 1] = arr[j]
             j -= 1
         arr[j + 1] = new_el
-
-
-#arr = [17, 4, 16, 3, 5, 11, 18, 26, 27, 19, 22, 13, 1, 20, 23, 7, 8, 15, 9, 6, 10, 24, 12, 14, 2, 21, 25]
-a = [5,6,7,4,1,2,3]
-#print(InsertionSort(arr, 0, 6))
-#print(KthOrderStatisticsStep(arr, 0, 26, 1))
-print(KthOrderStatisticsStep(a, 0, 6, 2))
