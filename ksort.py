f50a423a45ab134fb3cafce0c2729f80ae3705dd
konicaRu <53256405+konicaRu@users.noreply.github.com>
2020-03-02 20:45:23 +0300

    02_03_20

--- ksort.py
+++ ksort.py
@@ -1,14 +1,14 @@
 class ksort:
 
     def __init__(self):
-        self.items = [0] * 107  # создаем итоговый архив с нанами
+        self.items = [0] * 799  # создаем итоговый архив с нанами
 
     def index(self, s):
         if len(s) > 3 or len(s) < 3:
             return -1
         count_hach = 0
         res = 0
-        self.letter_hash = {'a': 1, 'b': 2, 'c': 3, 'd': 4, 'e': 5, 'f': 6, 'g': 7, 'h': 8}
+        self.letter_hash = {'a': 1, 'b': 101, 'c': 201, 'd': 301, 'e': 401, 'f': 501, 'g': 601, 'h': 701}
         self.ten_hash = {'0': 0, '1': 10, '2': 20, '3': 30, '4': 40, '5': 50, '6': 60, '7': 70, '8': 80, '9': 90}
         self.one_hash = {'0': 0, '1': 1, '2': 2, '3': 3, '4': 4, '5': 5, '6': 6, '7': 7, '8': 8, '9': 9}
         for i in s:
