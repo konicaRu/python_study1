de3caf2c4b38efd21c1623d4b30801a1b1f92d30
konicaRu <53256405+konicaRu@users.noreply.github.com>
2020-03-03 11:51:25 +0300

    03_02_20

--- test_ksort.py
+++ test_ksort.py
@@ -6,20 +6,20 @@ class MyTestCase(unittest.TestCase):
     def test_correct_values(self):
         count = 0
         k = ksort.ksort()
-        correct_values = ['a01', 'a02', 'a03', 'b33', 'c78', 'e45', 'f56', 'g89', 'h22']
+        correct_values = ['a01', 'a02', 'a03', 'b33', 'c78', 'e45', 'f56', 'g89', 'h22','h99']
         for i in correct_values:
             if k.add(i) == True:
                 count += 1
-        self.assertEqual(count == 9, True)
+        self.assertEqual(count == 10, True)
 
     def test_uncorrect_values(self):
         count = 0
         k = ksort.ksort()
-        uncorrect_values = ['a0154', 'aa02', 'z0a', 'a456', 'as98a','a', 'aa','123', 'g4']
+        uncorrect_values = ['f56','','f45a','aa5','a0154', 'aa02', 'z0a', 'a456', 'as98a','a', 'aa','123', 'g4']
         for i in uncorrect_values:
             if k.add(i) == False:
                 count += 1
-        self.assertEqual(count == 9, True)
+        self.assertEqual(count == 12, True)
 
 if __name__ == '__main__':
     unittest.main()
