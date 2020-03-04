83377251736c719634230350227c6fd3c1188d37
konicaRu <53256405+konicaRu@users.noreply.github.com>
2020-03-04 13:18:08 +0300

    04_03_20

--- BinarySearch.py
+++ BinarySearch.py
@@ -1,23 +1,24 @@
 class BinarySearch:
 
-    def __init__(self, arr):
-        self.Right = len(arr) - 1  # создаем итоговый архив с нанами
+    def __init__(self, a):
+        self.arr = a
+        self.Right = len(self.arr) - 1  # создаем итоговый архив с нанами
         self.Left = 0
 
     def Step(self, N):
         self.elem_find = 0
         while self.Left <= self.Right:
             pivot = (self.Left + self.Right) // 2
-            if  arr[pivot] == N: # было впереди arr[self.Left] == N or arr[self.Right] == N or
+            if  self.arr[pivot] == N: # было впереди self.arr[self.Left] == N or self.arr[self.Right] == N or
                 self.elem_find = True
                 return
             if self.Left == self.Right:#  if pivot == self.Left or self.Right == pivot:
                 self.elem_find = False
                 return
-            if arr[pivot] < N:
+            if self.arr[pivot] < N:
                 print(0)
                 self.Left = pivot + 1 # было без + 1
-            if arr[pivot] > N:
+            if self.arr[pivot] > N:
                 print(0)
                 self.Right = pivot - 1 # было без - 1
 
