376f7dc5b3497ae1f6e80171f739f8d7d3af75da
konicaRu <53256405+konicaRu@users.noreply.github.com>
2020-03-04 13:09:21 +0300

    04_03_20

--- BinarySearch.py
+++ BinarySearch.py
@@ -6,29 +6,24 @@ class BinarySearch:
 
     def Step(self, N):
         self.elem_find = 0
-        while True:
+        while self.Left <= self.Right:
             pivot = (self.Left + self.Right) // 2
-            if arr[self.Left] == N or arr[self.Right] == N or arr[pivot] == N:
+            if  arr[pivot] == N: # было впереди arr[self.Left] == N or arr[self.Right] == N or
                 self.elem_find = True
                 return
-            if self.Left == self.Right:
-                self.elem_find = False
-                return
-            if pivot == self.Left or self.Right == pivot:
+            if self.Left == self.Right:#  if pivot == self.Left or self.Right == pivot:
                 self.elem_find = False
                 return
             if arr[pivot] < N:
                 print(0)
-                self.Left = pivot
+                self.Left = pivot + 1 # было без + 1
             if arr[pivot] > N:
                 print(0)
-                self.Right = pivot
+                self.Right = pivot - 1 # было без - 1
 
     def GetResult(self):
         self.Step(N)
-
         if self.elem_find == True:
             return +1
         if self.elem_find == False:
             return -1
-
